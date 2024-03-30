f2750baf5b37843357c3a68b5de9b8fa449b7460
saberrytwo <saberrytwo@gmail.com>
2024-03-29 20:07:19 -0600

    update users seeder, commented out for now

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -10,17 +10,17 @@ use Illuminate\Database\Seeder;
 
 class UsersSeeder extends Seeder{
     public function run(){
-        User::truncate();
-        $users = [
-            [
-                'name' => 'Solomon Berry',
-                'email' => 'saberrytwo@gmail.com',
-                'email_verified_at' => null,
-                'password' => bcrypt('Password!234'),
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]
-            ];
-        User::insert($users);
+
+        // $users = [
+        //     [
+        //         'name' => 'Solomon Berry',
+        //         'email' => 'saberrytwo@gmail.com',
+        //         'email_verified_at' => null,
+        //         'password' => bcrypt('Password!234'),
+        //         'created_at' => Carbon::now(),
+        //         'updated_at' => Carbon::now()
+        //     ]
+        //     ];
+        // User::insert($users);
     }
 }
