944e84c92c357f6713e700f8984112616c9e7c96
saberrytwo <saberrytwo@gmail.com>
2024-03-29 21:31:12 -0600

    image url nullability checking

--- app/Http/Controllers/API/CharacterController.php
+++ app/Http/Controllers/API/CharacterController.php
@@ -17,7 +17,7 @@ class CharacterController extends BaseController
         foreach($characters as $character){
             $character->imageURL = $this->getS3Url($character->imageURL);
 
-            if($character->theme && $character->theme->imageURL){
+            if($character->theme && $character->theme->imageURL && $character->theme->imageURL !== null){
                 $character->theme->imageURL = $this->getS3Url($character->theme->imageURL);
             }
             
